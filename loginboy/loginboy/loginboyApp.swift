652eaa23995e2809b08f592c080aec4dc51d744f
danhorsley <danielhorsley@me.com>
2025-05-12 13:05:39 +0100

    template

---
+++ loginboy/loginboy/loginboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginboyApp.swift
+//  loginboy
+//
+//  Created by Daniel Horsley on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct loginboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ loginboy/loginboyTests/loginboyTests.swift
@@ -0,0 +1,16 @@
+//
+//  loginboyTests.swift
+//  loginboyTests
+//
+//  Created by Daniel Horsley on 12/05/2025.
+//
+
+import Testing
+
+struct loginboyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
